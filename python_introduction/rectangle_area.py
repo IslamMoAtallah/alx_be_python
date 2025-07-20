999e8f5ff63dbb8e09cda64fee83398c68a749ec
Islam Muhammad Atallah <59668025+IslamMoAtallah@users.noreply.github.com>
2025-07-20 13:21:53 +0300

    Update rectangle_area.py

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,4 +1,4 @@
-length = int(input("Enter rectangle length :"))
-width = int(input("Enter rectangle width:"))
+length = 10
+width = 5
 area = length * width 
-print(f"The area of the rectangle is: {area}")
+print("The area of the rectangle is: ", area)
