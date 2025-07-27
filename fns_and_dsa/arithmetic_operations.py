8709b95355fe8230c3f9e0a9042f04f7eb3f180f
Islam Muhammad Atallah <59668025+IslamMoAtallah@users.noreply.github.com>
2025-07-27 19:53:01 +0300

    arithmetic_operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+def perform_operation (num1, num2, operation ):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "substract":
+        return num1-num2 
+    elif operation == "multiply":
+        return num1*num2
+    elif operation == "divide":
+        if num2 == 0 :
+            return "Error division by zero "
+        return num1/num2
+    else :
+        return "Error: unsupported opertation"
+print(perform_operation(5, 2, "add"))
+
+
+
